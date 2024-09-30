152f491a2f6200356687696e13da24d4f33622bf
daweiyuanzhang <31055033+daweiyuanzhang@users.noreply.github.com>
2024-09-29 21:50:02 -0500

    Update linker.zsh

--- linker.zsh
+++ linker.zsh
@@ -1,3 +1,4 @@
 ln -sfn $ZAP_PLUGIN_DIR/dotfiles/config/alacritty.toml $HOME/.alacritty.toml; \
 ln -sfn $ZAP_PLUGIN_DIR/dotfiles/config/zellij.kdl     $HOME/.zellij.kdl; \
+ln -sfn $ZAP_PLUGIN_DIR/dotfiles/config/tmux.conf     $HOME/.tmux.conf; \
 ln -sfn $ZAP_PLUGIN_DIR/dotfiles/config/starship.toml  $HOME/.starship.toml; 
