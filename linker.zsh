e07589b1c059f458e32ff8e9de5037dde2795f85
daweiyuanzhang <31055033+daweiyuanzhang@users.noreply.github.com>
2024-11-06 19:46:08 +0800

    Update and rename linker.zsh to plugins/linker.zsh

--- linker.zsh
+++
@@ -1,4 +0,0 @@
-ln -sfn ${0:A:h}/config/alacritty.toml $HOME/.alacritty.toml; \
-ln -sfn ${0:A:h}/config/zellij.kdl     $HOME/.zellij.kdl;     \
-ln -sfn ${0:A:h}/config/tmux.conf      $HOME/.tmux.conf;      \
-ln -sfn ${0:A:h}/config/starship.toml  $HOME/.starship.toml; 
