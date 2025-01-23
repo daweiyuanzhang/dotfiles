038ead895ce2c1b9c4c5f9860e4d976053cb380f
daweiyuanzhang <31055033+daweiyuanzhang@users.noreply.github.com>
2025-01-23 14:33:55 +0800

    Update aliases.zsh

--- plugins/aliases.zsh
+++ plugins/aliases.zsh
@@ -5,8 +5,7 @@ alias .3='cd ../../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 alias ~='cd ~'
-alias --='cd -'
-
+alias -- -='cd -'
 
 # Folders Shortcuts
 [ -d ~/Downloads ]            && alias dl='cd ~/Downloads'
