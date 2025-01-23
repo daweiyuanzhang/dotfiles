97feb50df8037c542e6291d5861aadb4f6419bda
daweiyuanzhang <31055033+daweiyuanzhang@users.noreply.github.com>
2025-01-23 14:27:54 +0800

    Update aliases.zsh

--- plugins/aliases.zsh
+++ plugins/aliases.zsh
@@ -5,21 +5,14 @@ alias .3='cd ../../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 alias ~='cd ~'
-# alias -- -='cd -'
-# alias 1='cd -'
-# alias 2='cd -2'
-# alias 3='cd -3'
-# alias 4='cd -4'
-# alias 5='cd -5'
+alias -='cd -'
 
-# Easy report
-# command -v lsd &> /dev/null && alias ls="lsd"
-# alias l="ls"
-# alias la="ls -lA"
-# alias ll="ls -l"
-# alias lla="ls -lA"
-# lt() { lsd --tree --depth $1 2>/dev/null || lsd --tree --depth 1 }
-# lta() { lsd --tree --depth $1 -A 2>/dev/null || lsd --tree --depth 1 -A }
+
+# Folders Shortcuts
+[ -d ~/Downloads ]            && alias dl='cd ~/Downloads'
+[ -d ~/Desktop ]              && alias dt='cd ~/Desktop'
+
+# Shortcuts
 alias dud='du -d 1 -h'
 alias duf='du -sh *'
 alias fdir='find . -type d -name'
@@ -28,7 +21,6 @@ alias h='history'
 alias hgrep='history | grep'
 alias lgrep='ls -l | grep'
 alias lagrep='ls -lA | grep'
-alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,node_modules,tealdeer,Trash,vendor}'
 alias cp='cp -iv'
 alias mv='mv -iv'
 # alias ln='ln -iv'
@@ -46,8 +38,10 @@ alias disk='df -h | grep sd \
     | sed -e "s_9[0-9]%_\x1b[31m&\x1b[0m_" \
     | sed -e "s_/mnt/[-_A-Za-z0-9]*_\x1b[34;1m&\x1b[0m_"'
 
-# Shortcuts
 alias c='clear'
+alias q="~ && clear"
+# Show $PATH in readable view
+alias path='echo -e ${PATH//:/\\n}'
 
 command -v bat &> /dev/null && alias cat='bat'
 command -v bat &> /dev/null && alias catp='bat --style=plain'
@@ -57,7 +51,5 @@ command -v git-open &> /dev/null && alias gopen="git-open"
 # alias ch='echo > ~/.bash_history && echo > ~/.zsh_history'
 # alias cz='echo > ~/.z'
 alias x='echo "Goodbye! 👋" ; sleep 0.2; exit'
-
-# command -v htop &> /dev/null && alias htop-user='htop -u "$USER"'
 
 alias reloadzsh='source ~/.zshrc'
