14c243bb8d2e9249e63836ac37a257adc9a88f97
daweiyuanzhang <31055033+daweiyuanzhang@users.noreply.github.com>
2025-02-06 11:52:28 +0800

    Create uv.zsh

---
+++ plugins/uv.zsh
@@ -0,0 +1,15 @@
+# [U][V]: a modern and fast Python package and project manager
+
+alias uva='uv add'                # [A]dd: add a dependency to the project
+alias uvi='uv init'               # [I]nit: create a new Python project
+alias uvl='uv lock'               # [L]ock: create a lock file for the project's dependencies
+alias uvpyi='uv python install'   # [P]ip [I]nstall: manually install a Python Version (legacy)
+alias uvpyu='uv python uninstall' # [P]ip [I]nstall: manually install a Python Version (legacy)
+alias uvpi='uv pip install'       # [P]ip [I]nstall: manually install a dependency in the project (legacy)
+alias uvpu='uv pip uninstall'     # [P]ip [U]ninstall: manually uninstall a dependency from the project (legacy)
+alias uvpy='uv run python'        # [P]ython: run Python in the project's environment
+alias uvrm='uv remove'            # [R]e[M]ove: remove a dependency from the project
+alias uvru='uv run'               # [R][U]n: run a command in the project's environment
+alias uvs='uv sync'               # [S]ync: synchronize the project's dependencies with the environment
+alias uvt='uv tree'               # [T]ree: view the despendency tree for the project
+alias uvv='uv venv'               # [V]env: create a new virtual environment (legacy)
